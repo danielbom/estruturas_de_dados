4b7fe93a51afb4dcd13cc86fde4e295b9299ed40
danielbom <daniel.rodrigues.45@hotmail.com>
2018-08-20 06:39:11 -0300

    .

--- UtilTestes/utilitarios_teste.c
+++ UtilTestes/utilitarios_teste.c
@@ -1,22 +1,22 @@
 #include "../Util/utilitarios.h"
 
-long double fatorial(long double n)
+void printing(char *txt, double value)
 {
-    long double x = n;
-    while (--n)
-        x *= n;
-    return x;
+    printf("%s", txt);
+    printSimpleDouble(&value);
+    printf("\n");
 }
 
 int main()
 {
-    long double n = 170;
-    long double fact = fatorial(n);
-    printLongDouble(&n);
-    printf("\n");
-    printLongDouble(&fact);
-    //printSimpleDouble(&fact);
-    //printULLI(&fact);
+    double n = 10;
+    double k = 1;
+    double arrj = arrangement(n, k);
+    double comb = combination(n, k);
+
+    printing("n: ", n);
+    printing("arrj: ", arrj);
+    printing("comb: ", comb);
 
     return 0;
 }
