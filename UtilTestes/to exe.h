0c6107c42fb253e529a855c632ac8c7ca50b6458
danielbom <daniel.rodrigues.45@hotmail.com>
2018-08-20 06:38:42 -0300

    Para copiar e executar os arquivos testes.

---
+++ UtilTestes/to exe.h
@@ -0,0 +1,11 @@
+gcc array_teste.c -o array.exe && array.exe
+
+gcc bitwise_teste.c -o bitwise.exe && bitwise.exe
+
+gcc macros_teste.c -o macros.exe && macros.exe
+
+gcc more_string_teste.c -o strings.exe && strings.exe
+
+gcc primo_teste.c -o primo.exe && primo.exe
+
+gcc utilitarios_teste.c -o utilitarios.exe && utilitarios.exe
