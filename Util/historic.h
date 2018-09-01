7ba878c8f2b78ff3eb70c718f5690cf0636c9a12
danielbom <daniel.rodrigues.45@hotmail.com>
2018-09-01 11:44:55 -0300

    Adicionando comentários e algumas alterações sutis.

--- Util/historic.h
+++ Util/historic.h
@@ -7,18 +7,22 @@ char *HISTORIC[10] = {0};
 int index_historic = 0;
 int length_historic = 10;
 
+void add_historic(char *value); // Adiciona um valor ao historico
+char *get_historic(int pos);    // Recupera um valor do historico, dado uma posicao
+void free_historic();           // Libera os espaços alocados com o historico
+void print_historic();          // Imprime o estado atual do historico
+
 void add_historic(char *value)
 {
-    free(HISTORIC[index_historic]);
-    HISTORIC[index_historic] = strdup(value);
-    index_historic++;
-    index_historic %= length_historic;
+    free(HISTORIC[index_historic]);                          // Liberando o espaço para adicionar um item.
+    HISTORIC[index_historic] = strdup(value);                // Adicionando
+    index_historic = (index_historic + 1) % length_historic; // Incremento
 }
 
 char *get_historic(int pos)
 {
-    int i = index_historic - pos - 1;
-    return HISTORIC[(length_historic + i) % length_historic];
+    int i = index_historic - pos - 1;                         // Buscando a posição do index
+    return HISTORIC[(length_historic + i) % length_historic]; // Retornando o resultado
 }
 
 void free_historic()
