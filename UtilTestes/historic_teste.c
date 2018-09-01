cd8e4193741d355a8dfb38905513777fab576075
danielbom <daniel.rodrigues.45@hotmail.com>
2018-08-31 22:26:16 -0300

    Erros encontrados e concertados.

---
+++ UtilTestes/historic_teste.c
@@ -0,0 +1,47 @@
+#include "../Util/historic.h"
+
+int main()
+{
+    print_historic();
+    printf("\n");
+    add_historic("AAA");
+    print_historic();
+    printf("\n");
+    add_historic("BB");
+    print_historic();
+    printf("\n");
+    add_historic("C");
+    print_historic();
+    printf("\n");
+    add_historic("DDD");
+    print_historic();
+    printf("\n");
+    add_historic("EE");
+    print_historic();
+    printf("\n");
+    add_historic("F");
+    print_historic();
+    printf("\n");
+    add_historic("GGG");
+    print_historic();
+    printf("\n");
+    add_historic("HH");
+    print_historic();
+    printf("\n");
+    add_historic("I");
+    print_historic();
+    printf("\n");
+    add_historic("JJJ");
+    print_historic();
+    printf("\n");
+    add_historic("KK");
+    print_historic();
+    printf("\n");
+    add_historic("L");
+    print_historic();
+    printf("\n");
+    add_historic("MMM");
+    print_historic();
+    printf("\n");
+    return 0;
+}
