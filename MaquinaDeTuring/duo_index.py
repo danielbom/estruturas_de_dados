2a6d2bbaff0b8c5ced42fc96e0e6b8a4932f3a9a
danielbom <daniel.rodrigues.45@hotmail.com>
2018-09-03 23:24:18 -0300

    Transportado para outro repositório.

--- MaquinaDeTuring/duo_index.py
+++
@@ -1,9 +0,0 @@
-
-class duo_index(object):
-    def __init__(self, value):
-        self.value = value
-        self.prev = None
-        self.next = None
-
-    def __repr__(self):
-        return str(self.value)
