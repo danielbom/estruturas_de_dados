00f511543bb14a48ca484e81751293e7510c1ae5
danielbom <daniel.rodrigues.45@hotmail.com>
2018-11-11 06:38:08 -0200

    Definições para colorir o terminal no Ubuntu 16.04

---
+++ Util/color.h
@@ -0,0 +1,25 @@
+#ifndef COLORS
+#define COLORS
+
+#define RED_COLOR "\033[0;31m"
+#define BOLD_RED_COLOR "\033[1;31m"
+
+#define GREEN_COLOR "\033[0;32m"
+#define BOLD_GREEN_COLOR "\033[1;32m"
+
+#define YELLOW_COLOR "\033[0;33m"
+#define BOLD_YELLOW_COLOR "\033[1;33m"
+
+#define BLUE_COLOR "\033[0;34m"
+#define BOLD_BLUE_COLOR "\033[1;34m"
+
+#define MAGENTA_COLOR "\033[0;35m"
+#define BOLD_MAGENTA_COLOR "\033[1;35m"
+
+#define CYAN_COLOR "\033[0;36m"
+#define BOLD_CYAN_COLOR "\033[1;36m"
+
+#define END_COLOR "\033[0m"
+
+#define INT_COLOR BOLD_BLUE_COLOR "%d" END_COLOR
+#endif
