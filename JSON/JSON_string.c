cbaccc711f439e4b59fa5c9436523f0e37fba269
danielbom <daniel.rodrigues.45@hotmail.com>
2019-07-21 17:29:46 -0300

    Implementando string json.

---
+++ JSON/JSON_string.c
@@ -0,0 +1,27 @@
+#include "JSON_string.h"
+
+inline long long int JSON_string_size_value() {
+    return sizeof( char* );
+}
+
+// Constructor and destructor
+JSON_object_t* JSON_string_new( void ) {
+    return JSON_string_init( JSON_new() );
+}
+JSON_object_t* JSON_string_init( JSON_object_t* self ) {
+    memcpy( self, &JSON_string, JSON_string_size_value() );
+    self->value = calloc( 1, sizeof(char) );
+    return self;
+}
+void JSON_string_del( JSON_object_t** pself ) {
+    printf("(%s) Não implementado!", (*pself)->type);
+}
+
+// Getter and setter
+void* JSON_string_get( JSON_object_t* self ) {
+    return self->value;
+}
+void JSON_string_set( JSON_object_t* self, void* val ) {
+    free( self->value );
+    self->value = strdup( val );
+}
--- JSON/JSON_string.h
+++ JSON/JSON_string.h
@@ -8,11 +8,17 @@ JSON_object_t* JSON_string_new( void );
 JSON_object_t* JSON_string_init( JSON_object_t* self );
 void JSON_string_del( JSON_object_t** pself );
 
+// Getter and setter
+void* JSON_string_get( JSON_object_t* self );
+void  JSON_string_set( JSON_object_t* self, void* val );
+
 static const JSON_object_t JSON_string = {
     "String", 0,
     &JSON_string_new,
     &JSON_string_init,
-    &JSON_string_del
+    &JSON_string_del,
+    &JSON_string_get,
+    &JSON_string_set
 };
 
 
