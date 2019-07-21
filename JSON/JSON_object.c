c9e471087c347fabaf5a79be628b885d5d9a4bc0
danielbom <daniel.rodrigues.45@hotmail.com>
2019-07-21 17:38:12 -0300

    Implementando object json.

---
+++ JSON/JSON_object.c
@@ -0,0 +1,26 @@
+#include "JSON_object.h"
+
+inline long long int JSON_object_size_value() {
+    return sizeof( dict_t );
+}
+
+// Constructor and destructor
+JSON_object_t* JSON_object_new( void ) {
+    return JSON_object_init( JSON_new() );
+}
+JSON_object_t* JSON_object_init( JSON_object_t* self ) {
+    memcpy( self, &JSON_object, sizeof( JSON_object_t ) );
+    self->value = Dict.New( sizeof( JSON_object_t ) );
+    return self;
+}
+void JSON_object_del( JSON_object_t** pself ) {
+    printf("(%s) Não implementado!", (*pself)->type);
+}
+
+// Getter and setter
+void* JSON_object_get( JSON_object_t* self ) {
+    return self->value;
+}
+void  JSON_object_set( JSON_object_t* self, void* val ) {
+    memcpy( self->value, val, JSON_object_size_value() );
+}
--- JSON/JSON_object.h
+++ JSON/JSON_object.h
@@ -2,17 +2,24 @@
 #define OBJECT_JSON
 
 #include "JSON_interface.h"
+#include "../DataStructure/Dict/dict_t.h"
 
 // Constructor and destructor
 JSON_object_t* JSON_object_new( void );
 JSON_object_t* JSON_object_init( JSON_object_t* self );
 void JSON_object_del( JSON_object_t** pself );
 
+// Getter and setter
+void* JSON_object_get( JSON_object_t* self );
+void  JSON_object_set( JSON_object_t* self, void* val );
+
 static const JSON_object_t JSON_object = {
     "Object", 0,
     &JSON_object_new,
     &JSON_object_init,
-    &JSON_object_del
+    &JSON_object_del,
+    &JSON_object_get,
+    &JSON_object_set
 };
 
 #endif
