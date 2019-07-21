2995b7c6dc02a93c38df2a6c873415aa48e34e7f
danielbom <daniel.rodrigues.45@hotmail.com>
2019-07-21 16:53:27 -0300

    Criando a base para o objeto number json.

---
+++ JSON/JSON_number.c
@@ -0,0 +1,26 @@
+#include "JSON_number.h"
+
+inline long long int JSON_number_size_value() {
+    return sizeof( long long int );
+}
+
+// Constructor and destructor
+JSON_object_t* JSON_number_new( void ) {
+    return JSON_number_init( JSON_new() );
+}
+JSON_object_t* JSON_number_init( JSON_object_t* self ) {
+    memcpy( self, &JSON_number, sizeof( JSON_object_t ) );
+    self->value = calloc(1, JSON_number_size_value() );
+    return self;
+}
+void JSON_number_del( JSON_object_t** pself ) {
+    printf("(BOOL) Não implementado!");
+}
+
+// Getter and setter
+void* JSON_number_get( JSON_object_t* self ) {
+    return self->value;
+}
+void  JSON_number_set( JSON_object_t* self, void* val ) {
+    memcpy( self->value, val, JSON_number_size_value()  );
+}
--- JSON/JSON_number.h
+++ JSON/JSON_number.h
@@ -8,11 +8,17 @@ JSON_object_t* JSON_number_new( void );
 JSON_object_t* JSON_number_init( JSON_object_t* self );
 void JSON_number_del( JSON_object_t** pself );
 
+// Getter and setter
+void* JSON_number_get( JSON_object_t* self );
+void  JSON_number_set( JSON_object_t* self, void* val );
+
 static const JSON_object_t JSON_number = {
     "Object", 0,
     &JSON_number_new,
     &JSON_number_init,
-    &JSON_number_del
+    &JSON_number_del,
+    &JSON_number_get,
+    &JSON_number_set
 };
 
 
