815c1839bae222ce19ed0383658d1ba6d0c2c145
danielbom <daniel.rodrigues.45@hotmail.com>
2019-07-21 16:53:06 -0300

    Atualizando implementação do objeto boolean json.

--- JSON/JSON_boolean.c
+++ JSON/JSON_boolean.c
@@ -1,15 +1,19 @@
 #include "JSON_boolean.h"
 
+inline long long int JSON_boolean_size_value() {
+    return sizeof( char );
+}
+
 // Constructor and destructor
 JSON_object_t* JSON_boolean_new( void ) {
     return JSON_boolean_init( JSON_new() );
 }
 JSON_object_t* JSON_boolean_init( JSON_object_t* self ) {
     memcpy( self, &JSON_boolean, sizeof( JSON_object_t ) );
-    self->value = calloc( 1, sizeof( char ) );
+    self->value = calloc( 1, JSON_boolean_size_value() );
     return self;
 }
-void JSON_boolean_del( JSON_object_t** pself ){
+void JSON_boolean_del( JSON_object_t** pself ) {
     printf("(BOOL) Não implementado!");
 }
 
@@ -19,5 +23,5 @@ void* JSON_boolean_get( JSON_object_t* self ) {
     return self->value;
 }
 void JSON_boolean_set( JSON_object_t* self, void* val ) {
-    memcpy( self->value, val, sizeof(char) );
+    memcpy( self->value, val, JSON_boolean_size_value() );
 }
